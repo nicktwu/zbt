0c9857fe79958da1e0e66d8f9330431207cb64ba
Stanley Cen <jiecenzhao@gmail.com>
2017-08-22 14:45:47 -0700

    Add passport jwt authentication to all endpoints

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var auth = require('./auth');
+
 var users = require('./routes/users');
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser())
+app.use(cookieParser());
+
+app.use(auth.RequireLoggedIn);
 
 app.use('/api/v1/users', users);
 
---
+++ backend/auth.js
@@ -0,0 +1,21 @@
+var secrets = require('./secrets');
+var passport = require('passport');
+var JwtStrategy = require('passport-jwt').Strategy;
+var ExtractJwt = require('passport-jwt').ExtractJwt;
+
+var jwt_opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: secrets.crypto_key,
+};
+
+var jwt_strategy = new JwtStrategy(jwt_opts, function(payload, done) {
+  console.log(payload);
+  done(null, null);
+});
+
+passport.use(jwt_strategy);
+
+module.exports.RequireLoggedIn = passport.authenticate(
+  'jwt',
+  {session: false}
+);
