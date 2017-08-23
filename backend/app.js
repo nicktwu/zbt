61d4c0c724acb6598258ada410f37dfb0aa5094e
Stanley Cen <jiecenzhao@gmail.com>
2017-08-22 17:50:05 -0700

    Addd test data population route

--- backend/app.js
+++ backend/app.js
@@ -4,16 +4,36 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var auth = require('./auth');
-
+var db = require('./db');
 var users = require('./routes/users');
+var utils = require('./utils');
+
+if (utils.is_prod()) {
+  console.log("RUNNING IN PRODUCTION MODE...");
+} else {
+  console.log("RUNNING IN DEVELOPMENT MODE...");
+}
+
+db.init();
 
 var app = express();
+
+app.enable('trust proxy');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+if (!utils.is_prod()) {
+  app.get('/populate_test_data', function(req, res) {
+    db.populate_test_data();
+    res.send('ok');
+  });
+}
+
 app.use(auth.RequireLoggedIn);
+
+// PUT SECURE ROUTES AFTER THIS
 
 app.use('/api/v1/users', users);
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json({users: [{name: 'Timmy'}]});
+  res.json({users: [{name: req.user}]});
 });
 
 module.exports = router;
