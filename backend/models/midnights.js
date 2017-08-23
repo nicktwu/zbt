fefe86d2d701023105787e6c8bd318438f1ecd37
Stanley Cen <jiecenzhao@gmail.com>
2017-08-22 18:02:43 -0700

    Add semester and midnight models

---
+++ backend/models/midnights.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var midnightSchema = new Schema({
+  date: Date,
+  zebe: String,
+  task: String,
+  note: String,
+  feedback: String,
+  potential: Number,
+  awarded: Number,
+  reviewed: Boolean,
+});
+
+var midnightAccountSchema = new Schema({
+  semester: String,
+  zebe: String,
+  balance: Number,
+  requirement: Number,
+});
+
+var midnightTypeSchema = new Schema({
+  name: String,
+  value: Number,
+  desc: String,
+});
+
+var Midnight = mongoose.model('Midnight', midnightSchema);
+var MidnightAccount = mongoose.model('MidnightAccount', midnightAccountSchema);
+var MidnightType = mongoose.model('MidnightType', midnightTypeSchema);
+
+
+// TODO: implement midnight prefs
+
+module.exports = {
+  Midnight: Midnight,
+  MidnightAccount: MidnightAccount,
+  MidnightType: MidnightType,
+};
---
+++ backend/models/semester.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var semesterSchema = new Schema({
+  name: { type: String, index: true }, // e.g Fall 2017
+  start: Date,
+  end: Date,
+});
+
+var Semester = mongoose.model('Semester', semesterSchema);
+
+module.exports = Semester;
