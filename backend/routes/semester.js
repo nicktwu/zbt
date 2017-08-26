989f044994869496d2668955f6b507331bd480bd
Stanley Cen <jiecenzhao@gmail.com>
2017-08-26 04:25:29 -0700

    make semester 501 since i dont wanna implement it lol

--- backend/routes/semester.js
+++ backend/routes/semester.js
@@ -14,6 +14,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/create', function(req, res, next) {
   // TODO
+  res.sendStatus(501); // 501 not implemented
 });
 
 module.exports = router;
