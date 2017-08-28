18803c5e3523701e72178204a163657d89dcb0ec
deepg14 <deepankarg14@gmail.com>
2017-08-28 02:29:09 -1100

    profile view component added

--- frontend-prototype/src/newApp/components/ProfileView.js
+++ frontend-prototype/src/newApp/components/ProfileView.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 
-export default class CalendarView extends Component {
+export default class ProfileView extends Component {
   render() {
-    return (
-        <div>Profile View</div>
-    );
+ 	const { match } = this.props;
+	const { name } = match.name;
+	const { image } = match.image;
+	const { workWeekHours } = match.workWeekHours;
+	const { midnightPoints } = match.midnightPoints;
+	const { socialPoints } = match.socialPoints;
+	const { events } = match.events;
+	var assingmentsString = '';
+	var assignments = this.props.Assignments.map(function(tasks, index){
+        string += '<div>'
+        string += '<li key={index}>{tasks.eventName}</li>'
+        stirng += '<ul>'
+        string += '<li>{tasks.points} pts</li>'
+        string += '<li>{tasks.month} {tasks.date}, {tasks.year}</li>'
+        string += '</ul>'
+        stirng += '</div>' + '\n'
+    });
+
+	return (
+		<div>
+			<h3>{name}</h3>
+			<img src={image} />
+			<div>
+                <p>Workweek Hours: {workweekHours}</p>
+                <p>Midnight Points: {midnightPoints}</p>
+                <p>Social Points: {socialPoints}</p>
+            </div>
+            <div>{assignments}</div>
+		</div>
+	);
   }
 }
