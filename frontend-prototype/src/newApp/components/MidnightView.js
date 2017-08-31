001708d4542ec8f3ccd01a91868d6609879c34fb
Slava Kim <imslavko@gmail.com>
2017-08-31 03:03:38 -0400

    some css for midnight view

--- frontend-prototype/src/newApp/components/MidnightView.js
+++ frontend-prototype/src/newApp/components/MidnightView.js
@@ -24,16 +24,23 @@ class MidnightView extends Component {
       <div className="MidnightView">
         <div className="MidnightInfoPanel">
           <div className="MidnightView-title">
-            {event.task} - {event.date.toLocaleString('en-us', {weekday: 'short'})} {event.date.getMonth()}/{event.date.getDate()}
+            <h1>{event.task} - {event.date.toLocaleString('en-us', {weekday: 'short'})} {event.date.getMonth()}/{event.date.getDate()}</h1>
+          </div>
+          <div className="MidnightInfoPanel-stats">
+          <div className="MidnightInfoPanel-important">
+            <span className="MidnightInfoPanel-field">Assignee:</span> {event.zebe || "none"}
           </div>
           <div className="MidnightInfoPanel-important">
-            Assignee: {event.zebe || "none"}
+            <span className="MidnightInfoPanel-field">Points:</span> {event.potential}
           </div>
           <div className="MidnightInfoPanel-important">
-            Points: {event.potential} Reviewed: {event.reviewed ? <font color='00FF00'>True</font> : <font color='FF0000'>False</font>}
+            <span className="MidnightInfoPanel-field">Reviewed:</span>
+            {event.reviewed ? <font color='00FF00'> True</font> : <font color='FF0000'> False</font>}
+          </div>
           </div>
           <div className="MidnightInfoPanel-description">
-            this midnight involves doing: 
+            <div><h3>Description:</h3></div>
+            this midnight involves doing:
             <ul>
               <li>this work, </li>
               <li>that work, </li>
