8f80d118b3271727c607f743613e59ba948fae69
nwu <nwu@mit.edu>
2017-09-23 23:53:57 -0400

    Added makefile/dockerfile to make life easier

--- authentication/certificate/app.py
+++ authentication/certificate/app.py
@@ -44,6 +44,4 @@ def get_token():
 
 # For testing and development use only
 if __name__ == '__main__':
-    # monkeypatch
-    get_kerberos = lambda: 'testuser'
-    app.run(port=5555)
+    app.run(host='0.0.0.0',port=5555)
