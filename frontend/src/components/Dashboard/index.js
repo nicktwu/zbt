5c4ca6d6f91daaa10d9a4a848ec026e6d0ec3660
nwu <nwu@mit.edu>
2017-10-16 10:06:02 -0400

    Ship the v1.0.0 release

--- frontend/src/components/Dashboard/index.js
+++ frontend/src/components/Dashboard/index.js
@@ -150,7 +150,7 @@ class Dashboard extends Component {
           >
             <div className={classes.drawerInner}>
               <div className={classes.drawerHeader}>
-                <Typography type="title">ZBT, v1.0.beta</Typography>
+                <Typography type="title">ZBT, v1.0.0</Typography>
                 <IconButton onClick={this.handleDrawerClose}>
                   <ChevronLeftIcon />
                 </IconButton>
