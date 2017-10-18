05ddb4c6fbf6f9b64210c2d78f4962158202873d
nwu <nwu@mit.edu>
2017-10-18 13:55:10 -0400

    Switched midnight script processing from cursor

--- backend/reminders.js
+++ backend/reminders.js
@@ -8,18 +8,22 @@ remindMidnights = function() {
   var today = new Date();
   var firstDay = new Date(today.getFullYear(), today.getMonth(),today.getDate());
   var lastDay = new Date(firstDay.getFullYear(), firstDay.getMonth(), firstDay.getDate() + 1);
-  Midnight.Midnight.find({date: {$gte: firstDay, $lte: lastDay}}).cursor()
-    .on('data', function(doc) {
-      console.log("Reminding " + doc.zebe + " about " + doc.task + ".");
-      const addr = "nwu" + "@mit.edu";
-      const subj = "[ZBTodo Reminder] Midnight Tonight";
-      const body = "You have " + doc.task + " tonight";
-      emailer.send(addr, subj, body);
-    })
-    .on('error', function(err){
+  Midnight.Midnight.find({date: {$gte: firstDay, $lte: lastDay}}, function(err, docs) {
+    if (err) {
       console.log("Reminder sending failed.");
       console.log(err);
-    });
+    } else {
+      console.log("Found " + String(docs.length) + " docs")
+      for (var i = 0; i < docs.length; i++) {
+        var doc = docs[i];
+        console.log("Reminding " + doc.zebe + " about " + doc.task + ".");
+        const addr = "nwu" + "@mit.edu";
+        const subj = "[ZBTodo Reminder] Midnight Tonight";
+        const body = "You have " + doc.task + " tonight";
+        emailer.send(addr, subj, body);
+      }
+    }
+  });
 };
 
 module.exports = {
