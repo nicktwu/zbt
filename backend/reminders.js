ca1d4630283d0952b861730f3e8154bba8ba1364
nwu <nwu@mit.edu>
2017-10-18 14:00:00 -0400

    Force query execution

--- backend/reminders.js
+++ backend/reminders.js
@@ -8,12 +8,12 @@ remindMidnights = function() {
   var today = new Date();
   var firstDay = new Date(today.getFullYear(), today.getMonth(),today.getDate());
   var lastDay = new Date(firstDay.getFullYear(), firstDay.getMonth(), firstDay.getDate() + 1);
-  Midnight.Midnight.find({date: {$gte: firstDay, $lte: lastDay}}, function(err, docs) {
+  Midnight.Midnight.find({date: {$gte: firstDay, $lte: lastDay}}).exec(function(err, docs) {
     if (err) {
       console.log("Reminder sending failed.");
       console.log(err);
     } else {
-      console.log("Found " + String(docs.length) + " docs")
+      console.log("Found " + String(docs.length) + " docs");
       for (var i = 0; i < docs.length; i++) {
         var doc = docs[i];
         console.log("Reminding " + doc.zebe + " about " + doc.task + ".");
