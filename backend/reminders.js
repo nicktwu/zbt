266ef2d1d85f46de9660aa7b29b110b9831d718c
nwu <nwu@mit.edu>
2017-10-18 14:05:02 -0400

    Debugging

--- backend/reminders.js
+++ backend/reminders.js
@@ -5,25 +5,22 @@ var Midnight = require('./models/midnights');
 var emailer = require('./emailer');
 
 remindMidnights = function() {
+  console.log('hi');
   var today = new Date();
   var firstDay = new Date(today.getFullYear(), today.getMonth(),today.getDate());
   var lastDay = new Date(firstDay.getFullYear(), firstDay.getMonth(), firstDay.getDate() + 1);
-  Midnight.Midnight.find({date: {$gte: firstDay, $lte: lastDay}}).exec(function(err, docs) {
-    if (err) {
+  Midnight.Midnight.find({date: {$gte: firstDay, $lte: lastDay}}).cursor()
+    .on('data', function(doc) {
+      console.log("Reminding " + doc.zebe + " about " + doc.task + ".");
+      const addr = "nwu" + "@mit.edu";
+      const subj = "[ZBTodo Reminder] Midnight Tonight";
+      const body = "You have " + doc.task + " tonight";
+      emailer.send(addr, subj, body);
+    })
+    .on('error', function(err){
       console.log("Reminder sending failed.");
       console.log(err);
-    } else {
-      console.log("Found " + String(docs.length) + " docs");
-      for (var i = 0; i < docs.length; i++) {
-        var doc = docs[i];
-        console.log("Reminding " + doc.zebe + " about " + doc.task + ".");
-        const addr = "nwu" + "@mit.edu";
-        const subj = "[ZBTodo Reminder] Midnight Tonight";
-        const body = "You have " + doc.task + " tonight";
-        emailer.send(addr, subj, body);
-      }
-    }
-  });
+    });
 };
 
 module.exports = {
